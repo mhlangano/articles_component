92373b26f56dced5baaa30168546b3952fba8784
Khumalo <mhkhumalo@GISs-MacBook-Pro.local>
2017-08-24 20:17:45 +0200

    Displayed ArticleslList on component

--- src/Articles_Component/Articles_ComponentController.js
+++ src/Articles_Component/Articles_ComponentController.js
@@ -1,5 +1,9 @@
 ({
     doInit : function(component, event, helper) {
-
+        var action = component.get("c.getArticles");
+        action.setCallback(this, function(data) {
+        component.set("v.articles", data.getReturnValue());
+        });
+        $A.enqueueAction(action);
     }
 })
